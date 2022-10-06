987ad319925e773ecb8eb63c492b258f954e739d
Fabio Leandro <fabiomessias98@outlook.com>
2022-10-05 21:03:54 -0300

    Criado o custom API

--- inc/register-create-pages.php
+++ inc/register-create-pages.php
@@ -27,5 +27,18 @@ function ed_create_page() {
                 'icon_url'   => 'dashicons-excerpt-view' 
         ));
     }
+
+    if( function_exists('acf_add_options_page') ) {	
+        acf_add_options_page( 
+            array( 
+                'page_title' => 'API', 
+                'menu_title' => 'API', 
+                'menu_slug'  => 'api', 
+                'capability' => 'edit_posts', 
+                'position'   => '23,7', 
+                'redirect'   => false, 
+                'icon_url'   => 'dashicons-rest-api',
+        ));
+    }
 }
 add_action( 'init', 'ed_create_page' );
