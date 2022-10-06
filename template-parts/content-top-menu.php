5fea29340dabdf8256b47d1505200c70066bee63
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-05 21:13:49 -0300

    var dump

--- template-parts/content-top-menu.php
+++ template-parts/content-top-menu.php
@@ -4,39 +4,58 @@
 
         <div class="row">
 
-            <div class="col-12">
+            <?php
+                $link_pattern = get_field( 'link_padrao', 'option' );
+                $menu_post_link = $link_pattern . get_field( 'link_menu_editorias', 'option');
+                $request_posts = wp_remote_get( $menu_post_link );
+                 
+                if(!is_wp_error( $request_posts )) :
+                    $body = wp_remote_retrieve_body( $request_posts );
+                    $data = json_decode( $body );
 
-                <div class="row">
+                    if(!is_wp_error( $data )) :
+                        foreach( $data as $rest_post ) :
+            var_dump($rest_post);
 
-                    <div class="col-lg-3 d-flex justify-content-center align-items-center my-4 my-lg-0">
-                        <a href="<?php echo get_home_url( null, '/' ) ?>">
-                            <img
-                            class="img-fluid"
-                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/mercedarios-org.png"
-                            alt="Mercedários Org">
-                        </a>
-                    </div>
+            ?>
 
-                    <div class="col-lg-9 mb-2 mb-lg-0">
 
-                        <div class="row">
+                            <div class="col-12">
 
-                            <!-- loop -->
-                            <?php for( $i = 0; $i < 8; $i++ ) { ?>
-                                <div class="col-6 col-lg-3 px-1">
-                                    <a
-                                    class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-medium u-font-family-lato text-center text-decoration-none u-color-folk-white hover:u-bg-folk-dark-golden mb-1 py-2"
-                                    style="background-color: #A10F0F"
-                                    href="#">
-                                        Institucional
-                                    </a>
+                                <div class="row">
+
+                                    <div class="col-lg-3 d-flex justify-content-center align-items-center my-4 my-lg-0">
+                                        <a href="<?php echo get_home_url( null, '/' ) ?>">
+                                            <img
+                                            class="img-fluid"
+                                            src="<?php echo get_template_directory_uri()?>/../wp-bootstrap-starter-child/assets/images/mercedarios-org.png"
+                                            alt="Mercedários Org">
+                                        </a>
+                                    </div>
+
+                                    <div class="col-lg-9 mb-2 mb-lg-0">
+
+                                        <div class="row">
+
+                                        
+                                                <div class="col-6 col-lg-3 px-1">
+                                                    <a
+                                                    class="w-100 d-block u-font-size-14 xxl:u-font-size-16 u-font-weight-medium u-font-family-lato text-center text-decoration-none u-color-folk-white hover:u-bg-folk-dark-golden mb-1 py-2"
+                                                    style="background-color: #A10F0F"
+                                                    href="#">
+                                                        Institucional
+                                                    </a>
+                                                </div>
+                                        
+                                            <!-- end loop -->
+                                        </div>
+                                    </div>
                                 </div>
-                            <?php } ?>
-                            <!-- end loop -->
-                        </div>
-                    </div>
-                </div>
-            </div>
+                            </div>
+            <?php       endforeach;
+                    endif; 
+                endif; 
+            ?>
         </div>
     </div>
 </section>
