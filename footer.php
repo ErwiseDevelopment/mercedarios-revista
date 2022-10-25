c4b4d5cb60f76afcf3375bd8f181a2b00650c1ac
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-25 11:26:17 -0300

    correcao

--- footer.php
+++ footer.php
@@ -26,18 +26,8 @@
 
                     <div class="row justify-content-center">
 
-                        <div class="col-lg-4 mb-4 mb-lg-0">
-                            <a 
-                            class="d-block text-center py-3 px-2 text-decoration-none"
-                            style="background-color:#930000"
-                            href="">
-                                <span class="u-line-height-100 u-font-size-22 xxl:u-font-size-28 u-font-weight-bold u-font-family-cinzel u-color-folk-white"><?php echo get_field('nome_paroquia', 'option') ?></span> <br>
-                                <span class="u-font-size-12 u-font-weight-bold u-font-family-lato u-color-folk-white"><?php echo get_field ('local_paroquia', 'option') ?></span>
-                            </a>
-                        </div>
-
-                        <div class="col-10 col-lg-3">
-                            <a href="#">
+                          <div class="col-10 col-lg-3">
+                            <a href="<?php echo $link_pattern ?>">
                                 <img 
                                 class="img-fluid w-100 u-object-fit-cover"
                                 src="<?php echo get_field('logo', 'option')?>"
