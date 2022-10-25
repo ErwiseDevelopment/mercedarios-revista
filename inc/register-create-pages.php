394eb85c10566f7a18721b7084796b42493b146a
ErwiseDevelopment <erwisedev@gmail.com>
2022-10-25 10:37:31 -0300

    correcao

--- inc/register-create-pages.php
+++ inc/register-create-pages.php
@@ -15,30 +15,30 @@ function ed_create_page() {
         ));
     }
 
-    if( function_exists('acf_add_options_page') ) {	
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'Informativos', 
-                'menu_title' => 'Informativos', 
-                'menu_slug'  => 'Informativos', 
-                'capability' => 'edit_posts', 
-                //'position'   => '20,3', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-excerpt-view' 
-        ));
-    }
+    // if( function_exists('acf_add_options_page') ) {	
+    //     acf_add_options_page( 
+    //         array( 
+    //             'page_title' => 'Informativos', 
+    //             'menu_title' => 'Informativos', 
+    //             'menu_slug'  => 'Informativos', 
+    //             'capability' => 'edit_posts', 
+    //             //'position'   => '20,3', 
+    //             'redirect'   => false, 
+    //             'icon_url'   => 'dashicons-excerpt-view' 
+    //     ));
+    // }
 
-    if( function_exists('acf_add_options_page') ) {	
-        acf_add_options_page( 
-            array( 
-                'page_title' => 'API', 
-                'menu_title' => 'API', 
-                'menu_slug'  => 'api', 
-                'capability' => 'edit_posts', 
-                'position'   => '23,7', 
-                'redirect'   => false, 
-                'icon_url'   => 'dashicons-rest-api',
-        ));
-    }
+    // if( function_exists('acf_add_options_page') ) {	
+    //     acf_add_options_page( 
+    //         array( 
+    //             'page_title' => 'API', 
+    //             'menu_title' => 'API', 
+    //             'menu_slug'  => 'api', 
+    //             'capability' => 'edit_posts', 
+    //             'position'   => '23,7', 
+    //             'redirect'   => false, 
+    //             'icon_url'   => 'dashicons-rest-api',
+    //     ));
+    // }
 }
 add_action( 'init', 'ed_create_page' );
