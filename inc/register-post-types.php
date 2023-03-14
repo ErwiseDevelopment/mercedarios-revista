0a1023e0dc5be9d8c4a80906f3a0906b403590b3
ErwiseDevelopment <erwise.dev@gmail.com>
2023-03-14 15:34:07 -0300

    ajuste banner materiais

--- inc/register-post-types.php
+++ inc/register-post-types.php
@@ -83,4 +83,21 @@
 
 // }
 // add_action( 'init', 'erwise_create_taxonomy' );
+function ed_create_page(){
 
+if( function_exists('acf_add_options_page') ) {	
+    acf_add_options_page( 
+        array( 
+            'page_title' => 'Informativos', 
+            'menu_title' => 'Informativos', 
+            'menu_slug'  => 'Informativos', 
+            'capability' => 'edit_posts', 
+            //'position'   => '20,3', 
+            'redirect'   => false, 
+            'icon_url'   => 'dashicons-excerpt-view' 
+    ));
+}
+
+}
+
+add_action( 'init', 'ed_create_page' );
